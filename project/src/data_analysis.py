b80dd882551a0a44d2d88d499a34b20bd2b1e4d8
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2025-04-11 14:42:50 +0300

    Added function to get most common organization to data_analysis.py

--- project/src/data_analysis.py
+++ project/src/data_analysis.py
@@ -17,16 +17,40 @@ def load_json(file_url):
 def count_entries(data):
     return len(data)
 
-def generate_reports(data, txt_file, csv_file):
+def calculate_summary_statistics(data):
+    orgs = []
+
+    for entry in data:
+        orgs.append(entry['organisaatio'])
+
+    org_freq = {}
+
+    for org in orgs:
+        org_freq[org] = org_freq.get(org, 0) + 1
+
+    max_org = None
+    max_org_count = -1
+    for org, count in org_freq.items():
+        if count > max_org_count:
+            max_org = org
+            max_org_count = count
+
+    return {
+        'most_common_organization': max_org,
+    }
+
+def generate_reports(data, summary, txt_file, csv_file):
     try:
         with open(txt_file, 'w') as txt, open(csv_file, 'w') as csv:
             
             # For txt file
             txt.write(f"Total number of entries: {count_entries(data)}\n")
+            txt.write(f"Most common organization: {summary['most_common_organization']}\n")
             
             #For CSV file
             csv.write("Key,Value\n")
             csv.write(f"Total Entries,{count_entries(data)}\n")
+            csv.write(f"Most Common Organization,{summary['most_common_organization']}\n")
             
     except Exception as e:
         print(f"Error generating reports: {e}")
@@ -38,4 +62,10 @@ if __name__ == "__main__":
     
     if data:
         print(f"Number of entries: {count_entries(data)}")
-        generate_reports(data, 'report.txt', 'report.csv')
+        summary_statistics = calculate_summary_statistics(data)
+        
+        generate_reports(
+            data,
+            summary_statistics, 
+            'report.txt', 
+            'report.csv')
